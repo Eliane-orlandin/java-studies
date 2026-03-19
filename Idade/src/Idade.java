6d4327ad051cf5f6c22b90925a676cbaff0b74fe
Eliane Orlandin <liorlandin33@gmail.com>
2026-03-19 12:16:26 -0300

    feat: calcular idade com base no ano de nascimento

--- Idade/src/Idade.java
+++ Idade/src/Idade.java
@@ -1,17 +1,21 @@
+import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class Idade {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        var baseAno = OffsetDateTime.now().getYear();
         
         System.out.println("Qual é seu nome: ");
         String nome = scan.nextLine();
 
-        System.out.println("Qual é sua idade: ");
-        int idade = scan.nextInt();
+        System.out.println("Qual seu ano de nascimento: ");
+        int anoNascimento = scan.nextInt();
 
-        System.out.printf("Seu nome é %s e você tem %d anos\n", nome, idade);
+        int idade = baseAno - anoNascimento;
 
+        System.out.printf("Olá %s, você tem %d anos.\n", nome,idade);
 
         scan.close();
         
