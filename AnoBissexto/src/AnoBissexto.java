dc047a6dd2d2ae8e54a9c81e0cdbe6e2034b9871
Eliane Orlandin <liorlandin33@gmail.com>
2026-04-05 21:18:48 -0300

    feat: adicionar classe AnoBissexto para verificar anos bissextos

---
+++ AnoBissexto/src/AnoBissexto.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AnoBissexto {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        // Verifica se o ano é bissexto
+        if ((ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0)) {
+            System.out.println("O ano " + ano + " é bissexto.");
+        } else {
+            System.out.println("O ano " + ano + " não é bissexto.");
+        }
+
+        scanner.close();
+    }
+}
+
